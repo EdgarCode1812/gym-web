507a5aa9d70c4d6e3ceed56c5c1175143a515a7a
gym-web <edgar.cortes1812@gmail.com>
2023-05-23 14:07:35 -0600

    se agrego componente coachs

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import ComponentStart from './components/componentStart/ComponentStart'
 import Slider from './components/slider-gym/Slider'
+import Coachs from './components/coachs/Coachs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <Navbar />
     <ComponentStart />
     <Slider/>
+    <Coachs />
     </>
   )
 }
---
+++ src/components/coachs/Coachs.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import "./Coachs.css"
+
+export default function Coachs() {
+  return (
+    <div>
+            
+    <div className="section" id="trainer">
+      <h2 className="primary mb">Nuestros entrenadores profesionales</h2>
+      <div className="container flex">
+        <div className="trainer">
+          <img
+            src="https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/trainer1.jpg"
+            alt=""
+          />
+          <h3 className="secondary mb">Alan Smith</h3>
+          <p className="tertiary mb">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla.
+          </p>
+
+          <a href="#" className="btn-2">
+            <ion-icon name="arrow-redo-circle-outline"></ion-icon>
+          </a>
+        </div>
+
+        <div className="trainer">
+          <img
+            src="https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/trainer2.jpg"
+            alt=""
+          />
+          <h3 className="secondary mb">Alan Smith</h3>
+          <p className="tertiary mb">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla.
+          </p>
+
+          <a href="#" className="btn-2">
+            <ion-icon name="arrow-redo-circle-outline"></ion-icon>
+          </a>
+        </div>
+
+        <div className="trainer">
+          <img
+            src="https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/trainer3.jpg"
+            alt=""
+          />
+          <h3 className="secondary mb">Alan Smith</h3>
+          <p className="tertiary mb">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla.
+          </p>
+
+          <a href="#" className="btn-2">
+            <ion-icon name="arrow-redo-circle-outline"></ion-icon>
+          </a>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
